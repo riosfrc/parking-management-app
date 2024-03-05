5910df669c5d918fafad86d916e888ef5fbfcad0
riosfrc <rios.frc@gmail.com>
2024-03-04 19:10:35 -0600

    Blank Spring Boot project with dependencies

---
+++ src/main/java/com/riosfrc/parkingmanagementapp/ParkingManagementAppApplication.java
@@ -0,0 +1,13 @@
+package com.riosfrc.parkingmanagementapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingManagementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingManagementAppApplication.class, args);
+	}
+
+}
